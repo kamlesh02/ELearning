6a35468168a01a5b9aa88781d93795394a57a1ce
Kamlesh <104898283+kamlesh02@users.noreply.github.com>
2025-07-24 01:28:59 +0530

    Update purchaseApi.js

--- client/src/features/api/purchaseApi.js
+++ client/src/features/api/purchaseApi.js
@@ -1,7 +1,7 @@
 
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PURCHASE_API = "http://localhost:8080/api/v1/purchase";
+VITE_APP_BASE_URL="https://elearning-cp3p.onrender.com/api/v1"
 
 export const purchaseApi = createApi({
   reducerPath: "purchaseApi",
