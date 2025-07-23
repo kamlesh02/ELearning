0f074c08bfdb2b47ca24985859f97f3458b5d97c
Kamlesh <104898283+kamlesh02@users.noreply.github.com>
2025-07-24 01:27:36 +0530

    Update courseApi.js

--- client/src/features/api/courseApi.js
+++ client/src/features/api/courseApi.js
@@ -1,7 +1,7 @@
 
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_API = "http://localhost:8080/api/v1/course";
+VITE_APP_BASE_URL=https://elearning-cp3p.onrender.com/api/v1
 
 export const courseApi = createApi({
   reducerPath: "courseApi",
